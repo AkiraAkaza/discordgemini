094fdb267cbbe4e9398c86024e041fa684ab23d3
AkiraAkaza <137365140+AkiraAkaza@users.noreply.github.com>
2024-09-11 13:35:06 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const MODEL = "gemini-pro";
+const API_KEY = process.env.API_KEY
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const CHANNEL_ID = process.env.CHANNEL_ID;
+
+const ai = new GoogleGenerativeAI(API_KEY);
+const model = ai.getGenerativeModel({
+  model: MODEL,
+});
+
+const client = new Client({ intents: Object.keys(	GatewayIntentBits) });
+
+client.on('ready', async () => {
+  console.log(client.user.tag + ' Đang hoạt động!')
+  client.user.setActivity('GARENA: Liên Quân Mobile', {type: 'PLAYING'})
+})
+
+client.on("messageCreate", async (message) => {
+  try {
+    if (
+      !message.content ||
+      message.author.bot ||
+      message.channelId !== CHANNEL_ID
+    )
+      return;
+    const { response } = await model.generateContent(message.cleanContent);
+
+    await message.reply({
+      content: response.text(),
+      allowedMentions: {
+        parse: ['everyone','roles','users']
+      } 
+    });
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+client.login(BOT_TOKEN);
